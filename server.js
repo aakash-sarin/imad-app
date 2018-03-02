5bb0782b7002ad7f1b6fbd1dc729601e305c4d72
aakash-sarin <skybee9925@gmail.com>
2018-03-02 13:11:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,59 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title: 'Article One | Aakash Sarin',
+    heading: 'Article One',
+    date: 'March 02, 2018',
+    content: `
+                <p>
+                    Welcome to Article One. This is an experiment to help understand the strucutre of web pages.
+                </p>`
+};
+
+function createTemplate (data){
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    
+    var htmlTemplate = `
+        <html>
+            <head>
+            <title>
+                ${title}
+            </title>
+            <meta name="viewport" content="width-deivce-width, initial-scale=1"/>
+               <link href="/ui/style.css" rel="stylesheet" />
+            </head>
+            <body>
+                <div class=container>
+                <div>
+                    <a href=/>Home</a>
+                </div>
+                <hr/>
+                <h3>
+                    ${heading}
+                </h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    ${content}
+                </div>
+                </div>
+            </body>
+        </html>
+    `;
+    return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article_one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article_one.html'));
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/article_two', function (req, res) {
